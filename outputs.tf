c1816dfd0750ac6dfdadbd6adeba7b556edb0078
cheng527 <47427446+cheng527@users.noreply.github.com>
2022-03-06 16:19:52 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "endpoints" {
   description = "Array containing the full resource object and attributes for all endpoints created"
-  value       = module.endpoint.endpoint
+  value       = module.endpoints.endpoints
 }
